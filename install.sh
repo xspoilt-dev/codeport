dc3b05f378cc125220967dfc6c034d2b059a9c99
MINHAJUL ISLAM <peaky_xd@protonmail.com>
2025-01-17 00:18:55 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,28 @@
 GREEN="\033[0;32m"
 BLUE="\033[0;34m"
 YELLOW="\033[1;33m"
+RED="\033[0;31m"
 NC="\033[0m"
 
 echo -e "${YELLOW}Building Executable....${NC}"
 go build codeport.go
 
-
 if [ $? -eq 0 ]; then
-  echo -e "${BLUE}Moving executable to the bin folder...${NC}"
-  mv codeport ./bin/
-  echo -e "${GREEN}Build and move completed successfully!${NC}"
+  echo -e "${BLUE}Checking environment...${NC}"
+
+  if [ -d "$PREFIX" ]; then
+    echo -e "${BLUE}Detected Termux environment.${NC}"
+    mv codeport "$PREFIX/bin/codeport"
+  else
+    echo -e "${BLUE}Detected Linux environment.${NC}"
+    sudo mv codeport /usr/local/bin/codeport
+  fi
+
+  if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Build and move completed successfully!${NC}"
+  else
+    echo -e "${RED}Failed to move executable. Please check permissions.${NC}"
+  fi
 else
   echo -e "${RED}Build failed. Please check for errors.${NC}"
 fi
