c62531aad04d5bcbe5f492abb2f83c21242bdc71
MINHAJUL ISLAM <peaky_xd@protonmail.com>
2025-01-17 12:15:00 +0600

    Update codeport.go

--- codeport.go
+++ codeport.go
@@ -71,9 +71,27 @@ func uploadCode(filePath, language, title, description, password string) (string
 	return id, nil
 }
 
-func downloadCode(id, outputFile string) error {
-	pasteURL := fmt.Sprintf("%s/paste/%s", baseURL, id)
-	resp, err := http.Get(pasteURL)
+func downloadCode(id, outputFile, pwd string) error {
+	client := &http.Client{}
+	var resp *http.Response
+	var err error
+	var pasteURL = fmt.Sprintf("%s/paste/%s", baseURL, id)
+
+	if pwd == "" {
+		resp, err = http.Get(pasteURL)
+	} else {
+		formData := url.Values{}
+		formData.Set("password", pwd)
+
+		req, err := http.NewRequest("POST", pasteURL, strings.NewReader(formData.Encode()))
+		if err != nil {
+			return fmt.Errorf("\033[1;31mFailed to create request: %w\033[0m", err)
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		resp, err = client.Do(req)
+	}
+
 	if err != nil {
 		return fmt.Errorf("\033[1;31mFailed to fetch paste page: %w\033[0m", err)
 	}
@@ -81,16 +99,16 @@ func downloadCode(id, outputFile string) error {
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("\033[1;31mFailed to read HTML response: %w\033[0m", err)
+		return fmt.Errorf("\033[1;31mFailed to read response: %w\033[0m", err)
 	}
 	html := string(body)
-
 	downloadPattern := regexp.MustCompile(`href='([^"]+)'`)
 	match := downloadPattern.FindStringSubmatch(html)
 	if len(match) < 2 {
 		return fmt.Errorf("\033[1;31mFailed to extract download URL from HTML\033[0m")
 	}
 	downloadURL := match[1]
+
 	resp, err = http.Get(downloadURL)
 	if err != nil {
 		return fmt.Errorf("\033[1;31mFailed to download code file: %w\033[0m", err)
@@ -112,33 +130,61 @@ func downloadCode(id, outputFile string) error {
 	return nil
 }
 
+
 func main() {
-	filePath := flag.String("f", "", "\033[1;33mPath to the file to upload\033[0m")
-	downloadID := flag.String("g", "", "\033[1;33mPaste ID to download the file\033[0m")
-	outputFile := flag.String("o", "output.txt", "\033[1;33mPath to save the downloaded file\033[0m")
-	password := flag.String("p", "", "\033[1;33mPassword to protect the paste (optional)\033[0m")
-	description := flag.String("d", "No description", "\033[1;33mDescription of the paste (optional)\033[0m")
-	title := flag.String("t", "Untitled", "\033[1;33mTitle for the paste (optional)\033[0m")
-	language := flag.String("l", "python", "\033[1;33mProgramming language for syntax highlighting (default: python)\033[0m")
+	flag.Usage = func() {
+		fmt.Println("\033[1;31mUsage: \033[0m")
+		fmt.Println("\033[1;32m  -f [file]   \033[1;33m Upload a file\033[0m")
+		fmt.Println("\033[1;32m  -g [id]     \033[1;33m Download file by ID\033[0m")
+		fmt.Println("\033[1;32m  -p [pwd]    \033[1;33m Password for protected pastes\033[0m")
+		fmt.Println("\033[1;32m  -o [file]   \033[1;33m Output file for downloads\033[0m")
+		fmt.Println("\033[1;32m  -l [lang]   \033[1;33m Language for syntax highlighting\033[0m")
+		fmt.Println("\033[1;32m  -t [title]  \033[1;33m Title of the paste\033[0m")
+		fmt.Println("\033[1;32m  -d [desc]   \033[1;33m Description of the paste\033[0m")
+	}
+
+	filePath := flag.String("f", "", "Path to the file to upload")
+	downloadID := flag.String("g", "", "Paste ID to download the file")
+	outputFile := flag.String("o", "output.txt", "Path to save the downloaded file")
+	password := flag.String("p", "", "Password for protected pastes")
+	description := flag.String("d", "No description", "Description of the paste")
+	title := flag.String("t", "Untitled", "Title of the paste")
+	language := flag.String("l", "python", "Programming language for syntax highlighting")
 
 	flag.Parse()
+
 	info()
-	if *filePath != "" && *downloadID == "" {
+
+	if *filePath != "" {
+		if *downloadID != "" {
+			fmt.Println("\033[1;31mInvalid: Cannot upload and download at the same time!\033[0m")
+			flag.Usage()
+			return
+		}
 		fmt.Println("\033[1;32mUploading file to the server...\033[0m")
 		pasteID, err := uploadCode(*filePath, *language, *title, *description, *password)
 		if err != nil {
 			fmt.Printf("\033[1;31mError during upload: %s\033[0m\n", err)
 			return
 		}
-		fmt.Printf("\033[1;32mYour code has been successfully uploaded\033[0m\n\033[1;33mShare-Id: %s\033[0m\n\033[1;34mYou can get the code using 'codeport -g %s'\033[0m\n", pasteID, pasteID)
-	} else if *filePath == "" && *downloadID != "" {
+		if *password == "" {
+			fmt.Printf("\033[1;32mYour code has been successfully uploaded\033[0m\n\033[1;33mShare-Id: %s\033[0m\n\033[1;34mYou can get the code using 'codeport -g %s'\033[0m\n", pasteID, pasteID)
+
+		}else{
+			fmt.Printf("\033[1;32mYour code has been successfully uploaded\033[0m\n\033[1;33mShare-Id: %s\033[0m\n\033[1;34mYou can get the code using 'codeport -g %s -p %s'\033[0m\n", pasteID, pasteID, *password)
+		}
+		
+		return
+	}
+	if *downloadID != "" {
 		fmt.Println("\033[1;32mDownloading file from the server...\033[0m")
-		err := downloadCode(*downloadID, *outputFile)
+		err := downloadCode(*downloadID, *outputFile, *password)
 		if err != nil {
 			fmt.Printf("\033[1;31mError during download: %s\033[0m\n", err)
 		}
-	} else {
-		fmt.Println("\033[1;31mInvalid usage. Use -f to upload a file or -g to download using an ID.\033[0m")
-		flag.Usage()
+		return
 	}
+
+	fmt.Println("\033[1;31mInvalid usage. Use -f to upload a file or -g to download using an ID.\033[0m")
+	flag.Usage()
 }
